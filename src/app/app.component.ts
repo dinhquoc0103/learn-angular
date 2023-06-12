fa797e1496758d46db499f3e2119f30c2c53bfeb
quoc le <dinhquoc0103@gmail.com>
2023-06-12 15:20:53 +0700

    @ViewChild and @ViewChildren

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, QueryList, ViewChild, ViewChildren } from '@angular/core';
+import { HomeComponent } from './home/home.component';
 
 @Component({
   selector: 'app-root',
@@ -6,7 +7,28 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  logInName: string = 'admin';
+  // @ViewChild(HomeComponent, { static: true }) homeComp!: HomeComponent;
+  // @ViewChild('#homeComp') homeComp!: HomeComponent;
+  // @ViewChild('increaseBtn', { static: true }) increaseBtn!: ElementRef<HTMLButtonElement>;
+
+  @ViewChildren("homeComp") homeComp!: QueryList<HomeComponent>;
+
+  ngOnInit() {
+    // console.log('onInit:', this.homeComp);
+    // console.log('onInit:', this.increaseBtn.nativeElement);
+  }
+
+  ngAfterViewInit() {
+    // console.log('afterViewInit:', this.homeComp);
+    // console.log('onInit:', this.increaseBtn.nativeElement);
+    // console.log(this.homeComp.first);
+
+    this.homeComp.forEach(element => {
+      console.log(element);
+
+    });
+
+  }
 }
 
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,16 +4,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FormsModule } from '@angular/forms';
-
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    HomeComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule
+    FormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
